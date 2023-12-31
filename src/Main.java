9402cfeeb0ded608db3bfa21423995c4bc70c0a7
Mudia Osagie <mudia.code@gmail.com>
2023-12-31 15:20:11 +0000

    add UniqueID class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("University Register!");
+        //classname var = new classname()
+        Student s1 = new Student();
+        s1.firstName =  "Mudia";
+        s1.lastName = "Osagie";
+        s1.studentID();
+        s1.course = "Computer Science";
+        s1.Module = "Functional Programming";
+
+        System.out.println(s1.studentID);
     }
 }
 
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,14 @@
 public class Student {
-    String name;
-    int StudentID;
+    String firstName;
+    String lastName;
+    int studentID;
     String course;
     String Module;
+
+    Student(){
+    }
+
+    public void studentID() {
+        studentID = UniqueID.generateUniqueIdentifier();
+    }
 }
