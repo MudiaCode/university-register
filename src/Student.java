8e64a7b5166d1e0812cf9ed1bf1ebb8628f2c8f6
Mudia Osagie <mudia.code@gmail.com>
2024-01-02 19:26:36 +0000

    add studentRecord

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,45 @@
+import javax.xml.namespace.QName;
+import java.util.List;
+
 public class Student {
-    String firstName;
-    String lastName;
-    int studentID;
-    String course;
-    String Module;
 
-    Student(){
+    private String name;
+    private int studentID;
+
+    public String getName() {
+        return name;
     }
 
-    public void studentID() {
-        studentID = UniqueID.generateUniqueIdentifier();
+    public void setName(String name) {
+        this.name = name;
     }
-}
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    private String course;
+    private String module;
+
+    }
+
--- src/UniqueID.java
+++ src/UniqueID.java
@@ -2,9 +2,6 @@ import java.util.Random;
 
 public class UniqueID{
     private static final Random random = new Random(1);
-    public static void main(String[] args) {
-        Integer UniqueID = generateUniqueIdentifier();
-    }
 
     public static Integer generateUniqueIdentifier() {
         return random.nextInt(900000) + 100000;
